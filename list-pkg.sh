ea1873fa5a75832aa4f303c7284e72c5e3ca3d1f
Abel <abel.s.siqueira@gmail.com>
2021-03-19 10:24:00 -0300

    Prepare for breakage

---
+++ list-pkg.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "| Package name | master | stable |"
+echo "|--|--|--|"
+count=0
+for file in *
+do
+  [ "$file" == "list-pkg.sh" ] && continue
+  if [ $count == "0" ]; then
+    name=$(echo $file | cut -f1 -d-)
+    echo -n "| $name | "
+  else
+    echo -n "| "
+  fi
+  cat $file
+  if [ $count == "0" ]; then
+    echo -n " "
+    count=1
+  else
+    echo " |"
+    count=0
+  fi
+done
